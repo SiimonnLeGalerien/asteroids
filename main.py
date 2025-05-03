137aef1e8bb6e9ef03704dbcb23c78e8313247e4
SiimonnLeGalerien <morel.simon77@gmail.com>
2025-05-03 15:34:06 +0200

    adding collisions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -13,14 +15,23 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        updatable.update(dt)
+        for upt in updatable:
+            upt.update(dt)
+        for ast in asteroids:
+            if player.collides(ast):
+                print("Game Over!")
+                return
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
